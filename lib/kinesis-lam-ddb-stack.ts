79a015269deec8ba22743947b41f61ac57dd057a
EC2 Default User <ec2-user@ip-172-31-41-47.eu-west-1.compute.internal>
2023-04-25 12:57:24 +0000

    initial version

---
+++ lib/kinesis-lam-ddb-stack.ts
@@ -0,0 +1,84 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ddb from "aws-cdk-lib/aws-dynamodb";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as kinesis from "aws-cdk-lib/aws-kinesis";
+import {
+  KinesisEventSource,
+  SqsDlq,
+} from "aws-cdk-lib/aws-lambda-event-sources";
+
+export class KinesisLamDdbStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const ddbUnicornSensorData = new ddb.Table(this, "UnicornSensorData", {
+      partitionKey: {
+        name: "Name",
+        type: ddb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "StatusTime",
+        type: ddb.AttributeType.STRING,
+      },
+      tableName: "UnicornSensorData",
+      billingMode: ddb.BillingMode.PROVISIONED,
+      readCapacity: 5,
+      writeCapacity: 5,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // ** NOT recommended for production code **
+    });
+
+    const deadLetterQueue = new sqs.Queue(this, "wildrydes-queue", {
+      queueName: "wildrydes-queue",
+    });
+
+    // Use an EXISTING role
+    const lambdaRole = iam.Role.fromRoleArn(
+      this,
+      "Existing-WildRydesStreamProcessorRole",
+      "arn:aws:iam::174543029707:role/WildRydesStreamProcessorRole"
+    );
+
+    const lambdaFn = new lambda.Function(this, "Function", {
+      functionName: "WildRydesStreamProcessor",
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      deadLetterQueueEnabled: true,
+      deadLetterQueue: deadLetterQueue,
+      code: lambda.Code.fromAsset(join(__dirname, "../lambda")),
+      role: lambdaRole,
+      environment: {
+        TABLE_NAME: ddbUnicornSensorData.tableName,
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      },
+    });
+    const stream = kinesis.Stream.fromStreamArn(
+      this,
+      "ExistingStream",
+      "arn:aws:kinesis:eu-west-1:174543029707:stream/wildrydes"
+    );
+    lambdaFn.addEventSource(
+      new KinesisEventSource(stream, {
+        batchSize: 100, // default
+        startingPosition: lambda.StartingPosition.LATEST,
+        onFailure: new SqsDlq(deadLetterQueue),
+        retryAttempts: 0,
+      })
+    );
+
+    new cdk.CfnOutput(this, "DeadLetterQueue.ARN:", {
+      value: deadLetterQueue.queueArn,
+    });
+
+    new cdk.CfnOutput(this, "UnicornSensorData.ARN:", {
+      value: ddbUnicornSensorData.tableArn,
+    });
+    new cdk.CfnOutput(this, "LambdaFn.ARN", { value: lambdaFn.functionArn });
+    new cdk.CfnOutput(this, "LambdaFn.Role.ARN", {
+      value: lambdaFn.role!.roleArn,
+    });
+  }
+}
---
+++ test/kinesis-lam-ddb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KinesisLamDdb from '../lib/kinesis-lam-ddb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kinesis-lam-ddb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KinesisLamDdb.KinesisLamDdbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
